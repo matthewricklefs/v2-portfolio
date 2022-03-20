44fc8231bd8d5414c2fbeab40a6e0f933328683e
Matthew <ricklefsmatthew1003@gmail.com>
2022-03-20 15:58:10 -0400

    configures eslint, prettier, creates babelrc, creates utils directory, scrollreveal config fil, creates configuration file for project details

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,67 +1,40 @@
 module.exports = {
-  email: 'ricklefsmatthew1003@gmail.com',
-
-  socialMedia: [
-    {
-      name: 'GitHub',
-      url: 'https://github.com/matthewricklefs',
-    },
-    {
-      name: 'Instagram',
-      url: '',
-    },
-    {
-      name: 'Twitter',
-      url: '',
-    },
-    {
-      name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
-    },
-    {
-      name: 'Codepen',
-      url: '',
-    },
-  ],
-
-  navLinks: [
-    {
-      name: 'About',
-      url: '/#about',
-    },
-    {
-      name: 'Experience',
-      url: '/#jobs',
-    },
-    {
-      name: 'Work',
-      url: '/#projects',
-    },
-    {
-      name: 'Contact',
-      url: '/#contact',
-    },
-  ],
-
-  colors: {
-    green: '#64ffda',
-    navy: '#0a192f',
-    darkNavy: '#020c1b',
+  siteMetadata: {
+    title: "Matthew Ricklefs",
+    description:
+      "Matthew Ricklefs is a software engineer who specializes in building, contributing toward, and maintaining exceptional digital experiences.",
+    siteUrl: "https://www.yourdomain.tld",
+    image: "/TODO",
   },
-
-  srConfig: (delay = 200, viewFactor = 0.25) => ({
-    origin: 'bottom',
-    distance: '20px',
-    duration: 500,
-    delay,
-    rotate: { x: 0, y: 0, z: 0 },
-    opacity: 0,
-    scale: 1,
-    easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
-    mobile: true,
-    reset: false,
-    useDelay: 'always',
-    viewFactor,
-    viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
-  }),
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
 };
