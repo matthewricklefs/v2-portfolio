f0cd9d831de334a908a09352129bbe431b94d35f
Matthew <ricklefsmatthew1003@gmail.com>
2022-03-20 18:07:43 -0400

    adds featured/ and posts/ to content, debugs gatsby&graphql config and development instance, debugs graphql

--- src/templates/post.js
+++ src/templates/post.js
@@ -4,7 +4,7 @@ import kebabCase from 'lodash/kebabCase';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import styled from 'styled-components';
-import { Layout } from '@components';
+import { Layout } from '../components';
 
 const StyledPostContainer = styled.main`
   max-width: 1000px;
