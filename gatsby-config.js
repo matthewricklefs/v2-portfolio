a0aa60f859b83d839469188e168496683b8e926f
Matthew <ricklefsmatthew1003@gmail.com>
2022-03-20 15:40:55 -0400

    creates base layout for portfolio sections inside of components/sections/

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,67 @@
 module.exports = {
-  siteMetadata: {
-    title: `new`,
-    siteUrl: `https://www.yourdomain.tld`
+  email: 'ricklefsmatthew1003@gmail.com',
+
+  socialMedia: [
+    {
+      name: 'GitHub',
+      url: 'https://github.com/matthewricklefs',
+    },
+    {
+      name: 'Instagram',
+      url: '',
+    },
+    {
+      name: 'Twitter',
+      url: '',
+    },
+    {
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/bchiang7',
+    },
+    {
+      name: 'Codepen',
+      url: '',
+    },
+  ],
+
+  navLinks: [
+    {
+      name: 'About',
+      url: '/#about',
+    },
+    {
+      name: 'Experience',
+      url: '/#jobs',
+    },
+    {
+      name: 'Work',
+      url: '/#projects',
+    },
+    {
+      name: 'Contact',
+      url: '/#contact',
+    },
+  ],
+
+  colors: {
+    green: '#64ffda',
+    navy: '#0a192f',
+    darkNavy: '#020c1b',
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+
+  srConfig: (delay = 200, viewFactor = 0.25) => ({
+    origin: 'bottom',
+    distance: '20px',
+    duration: 500,
+    delay,
+    rotate: { x: 0, y: 0, z: 0 },
+    opacity: 0,
+    scale: 1,
+    easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+    mobile: true,
+    reset: false,
+    useDelay: 'always',
+    viewFactor,
+    viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+  }),
 };
