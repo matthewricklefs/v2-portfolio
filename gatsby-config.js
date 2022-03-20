cb3f8dfef4278d8cf5fa89314ed907623f4988b0
Matthew <ricklefsmatthew1003@gmail.com>
2022-03-20 16:51:49 -0400

    integrates footer and menu template, inserts static profile image, updates gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,40 +1,40 @@
 module.exports = {
   siteMetadata: {
-    title: "Matthew Ricklefs",
+    title: 'Matthew Ricklefs',
     description:
-      "Matthew Ricklefs is a software engineer who specializes in building, contributing toward, and maintaining exceptional digital experiences.",
-    siteUrl: "https://www.yourdomain.tld",
-    image: "/TODO",
+      'Matthew Ricklefs is a software engineer who specializes in building, contributing toward, and maintaining exceptional digital experiences.',
+    siteUrl: 'https://www.yourdomain.tld',
+    image: '/TODO',
   },
   plugins: [
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: "src/images/icon.png",
+        icon: 'src/images/icon.png',
       },
     },
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: 'pages',
+        path: './src/pages/',
       },
-      __key: "pages",
+      __key: 'pages',
     },
   ],
 };
